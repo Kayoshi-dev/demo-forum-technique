a109dad151327e83ab3c3a1a68188c7e19bb10f8
Kayoshi-dev <maxime.jme@gmail.com>
2022-01-09 19:10:38 +0100

    Fix broken thumbnail on production..

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,22 +1,3 @@
-if(process.env.NODE_ENV === "production") {
-  module.exports = [
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            upgradeInsecureRequests: null,
-          },
-        },
-      },
-    },
-  ];
-}
-
 module.exports = [
   'strapi::errors',
   'strapi::security',
