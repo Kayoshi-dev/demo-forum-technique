db1465a425de4ccd6332c3f21520eb7b4a15ee87
Kayoshi-dev <maxime.jme@gmail.com>
2022-01-09 01:51:56 +0100

    Fix broken link image

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -7,5 +7,5 @@ export function getApiUrl() {
 }
 
 export function getEnvUrl() {
-    return process.env.NODE_ENV === "production" ? "https://demo-ft-strapi.herokuapp.com" : "http://localhost:1337"
+    return process.env.NODE_ENV === "production" ? "https://demo-ft-strapi.herokuapp.com" : ""
 }
